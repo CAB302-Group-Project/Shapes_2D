a61e53af0fe606930f6f3c3f8b1a3a207bc3360d
Jay Prakash <161402836+CommitRegretRepeat@users.noreply.github.com>
2025-03-30 15:17:22 +1000

    Nearly finished with Circle.java, just some minor issues with Java recognising the Circle class as abstract

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -3,25 +3,63 @@
  */
 package shapes;
 
+import java.awt.Point;
+
 /**
  * Class to represent a circle shape - as this is a circle it does not
  * contain any vertices
  */
 
-public class Circle  {
+public class Circle extends Shape2D {
+    //Private set
+    private double radius;
+    private Point centre;
 
-
+    //Public get
+    public double getRadius()
+    {
+        return radius;
+    }
 
     /**
      * Constructor for Circle shape object
      * @param centre The centre of the circle represented as a Point object
      * @param radius The radius of the circle created
      */
-    public Circle(Point centre, double radius) {
 
+    //Constructor for new circle object
+    public Circle(Point centre, double radius)
+    {
+        super(centre);
+        this.centre = (centre);
+        this.radius = radius;
     }
 
+    @Override
+    public double getArea()
+    {
+        return Math.PI * Math.pow(radius, 2); // Area = Pi * r^2
+    }
 
+    @Override
+    public double getPerimeter()
+    {
+        return Math.PI * radius * 2; // Perimeter = 2 * Pi * r
+    }
 
+    public Point[] getVertices()
+    {
+        return new Point[]{}; // Returns an empty array of vertices because a circle has none
+    }
+
+    @Override
+    public boolean containsPoint(Point point)
+    {
+        // Using pythagoras to find the distance between the centre of the circle and a provided point
+        double delta_x = point.getX() - centre.getX(); // Change in horizontal
+        double delta_y = point.getY() - centre.getY(); // Change in vertical
+        double distance_squared = Math.pow(delta_x, 2) + Math.pow(delta_y, 2); // a^2 + b^2 = c^2
+        return radius >= Math.pow(distance_squared, 0.5); // Testing if the distance falls within the radius of the circle
+    }
 
 }
--- src/shapes/Shape2D.java
+++ src/shapes/Shape2D.java
@@ -3,6 +3,8 @@
  */
 package shapes;
 
+import java.awt.Point;
+
 /**
  * Shape2D is an Abstract Class and acts as a base class for all the 2D shapes
  * which includes Equilateral Triangle, Circle, Rectangle amd Square.
@@ -10,9 +12,30 @@ package shapes;
  * Shape2D
  */
 public abstract class Shape2D {
+    protected double area;
+    protected double perimeter;
 
-
-    public Shape2D(Point centre) {
+    public Shape2D(Point centre)
+    {
 
     }
+
+    void translate(double area, double perimeter)
+    {
+
+    }
+
+    public double getArea()
+    {
+        return this.area;
+    }
+
+    public double getPerimeter()
+    {
+        return this.perimeter;
+    }
+
+    public abstract Point[] getVertices();
+
+    public abstract boolean containsPoint(Point point);
 }
